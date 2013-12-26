8fe20f3e50735a95be38626eb29c0259fff51b61
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2013-12-25 20:06:24 -0500

    more testing

--- test/jsonp.test.js
+++ test/jsonp.test.js
@@ -14,7 +14,7 @@ var assert = require('chai').assert
   , jsonp = require('../')
 
 
-describe('JSONTransport()', function () {
+describe('jsonp()', function () {
   before(function (done) {
     app.use(jsonp({ callbackName: 'my_cb_name' }))
     app.use(mount('/buffered', function *() {
@@ -29,55 +29,63 @@ describe('JSONTransport()', function () {
   })
 
   it('shouldn\'t do anything if callback is not provided / GET', function (done) {
-    get('http://localhost:3000/buffered', function (err, response, body) {
+    get('http://localhost:3000/buffered', function (err, res, body) {
       assert.equal(body.foo, 'bar')
-      assert.equal(response.headers['content-type'], 'application/json')
+      assert.equal(res.headers['content-type'], 'application/json')
+      done(err)
+    })
+  })
+
+  it('shouldn\'t do anything if callback is not provided / GET / Stream', function (done) {
+    get('http://localhost:3000/streaming', function (err, res, body) {
+      assert.lengthOf(body, 5)
+      assert.equal(res.headers['content-type'], 'application/octet-stream')
       done(err)
     })
   })
 
   it('shouldn\'t do anything if callback is not provided / POST', function (done) {
-    post('http://localhost:3000/buffered', function (err, response, body) {
+    post('http://localhost:3000/buffered', function (err, res, body) {
       assert.equal(body.foo, 'bar')
-      assert.equal(response.headers['content-type'], 'application/json')
+      assert.equal(res.headers['content-type'], 'application/json')
       done(err)
     })
   })
 
   it('should switch to JSONP mode if callback is provided', function (done) {
-    get('http://localhost:3000/buffered?my_cb_name=cb', function (err, response, body) {
-      assert.equal(body, ';cb({\n  "foo": "bar"\n});')
-      assert.equal(response.headers['content-type'], 'text/javascript')
+    get('http://localhost:3000/buffered?my_cb_name=cb', function (err, res, body) {
+      var data = JSON.parse(body.match(/cb\(([^)]+)\)/m)[1])
+      assert.equal(data.foo, 'bar')
+      assert.equal(res.headers['content-type'], 'text/javascript')
       done(err)
     })
   })
 
   it('should switch to JSONP+iframe mode if callback is provided / POST', function (done) {
-    post('http://localhost:3000/buffered?my_cb_name=cb', function (err, response, body) {
+    post('http://localhost:3000/buffered?my_cb_name=cb', function (err, res, body) {
       var data = JSON.parse(body.match(/cb\(([^)]+)\)/m)[1])
       assert.equal(data.foo, 'bar')
       assert.match(body, /<!doctype html>/)
-      assert.equal(response.headers['content-type'], 'text/html; charset=utf-8')
+      assert.equal(res.headers['content-type'], 'text/html; charset=utf-8')
       done(err)
     })
   })
 
-  it('should switch to JSONP mode if callback is provided / Stream', function (done) {
-    get('http://localhost:3000/streaming?my_cb_name=cb', function (err, response, body) {
-      body = JSON.parse(body.replace(';cb(', '').replace(');', ''))
-      assert.lengthOf(body, 5)
+  it('should switch to JSONP mode if callback is provided / GET / Stream', function (done) {
+    get('http://localhost:3000/streaming?my_cb_name=cb', function (err, res, body) {
+      var data = JSON.parse(body.match(/cb\(([^)]+)\)/m)[1])
+      assert.lengthOf(data, 5)
+      assert.equal(res.headers['content-type'], 'text/javascript')
       done(err)
-    }).once('data', function (chunk) {
-      assert.equal(chunk.toString('utf8'), ';cb(')
     })
   })
 
   it('should switch to JSONP+iframe mode if callback is provided / POST / Stream', function (done) {
-    post('http://localhost:3000/streaming?my_cb_name=cb', function (err, response, body) {
+    post('http://localhost:3000/streaming?my_cb_name=cb', function (err, res, body) {
       var data = JSON.parse(body.match(/cb\(([^)]+)\)/m)[1])
       assert.lengthOf(data, 5)
       assert.match(body, /<!doctype html>/)
-      assert.equal(response.headers['content-type'], 'text/html; charset=utf-8')
+      assert.equal(res.headers['content-type'], 'text/html; charset=utf-8')
       done(err)
     })
   })
