456e511f1dd5875912d872d3823ffbea5422a89f
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2013-12-27 10:20:03 -0500

    removed legacy GLOBAL.LIB_PATH

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/**
+/*!
  * index.js
  * Created by Kilian Ciuffolo on Dec 25, 2013
  * Copyright (c) 2013 Kilian Ciuffolo, me@nailik.org
  */
 
 /**
- * Commodity globals
- */
-
-GLOBAL.LIB_PATH = __dirname + (process.env.AI_COV ? '/lib-cov' : '/lib')
-
-/**
  * Main exports
  */
 
-module.exports = require(LIB_PATH + '/jsonp.js')
+module.exports = require((process.env.KOA_JSONP_COV ? './lib-cov' : './lib') + '/jsonp')
